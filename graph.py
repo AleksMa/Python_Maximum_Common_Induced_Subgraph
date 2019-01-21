7b26545ae39c3f9d4470a2b6167143862b64d973
mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>
2019-01-22 00:43:11 +0100

    Signed-off-by: mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>

--- graph.py
+++ graph.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import networkx as nx
 import warnings
+import itertools as it
 import matplotlib.cbook
 warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
 
@@ -106,7 +107,7 @@ def display_graph(graph):
     else :
         G = nx.Graph(graph._graphdic)
         
-    pos=nx.spring_layout(G,k=2,seed = 69)
+    pos=nx.spring_layout(G,seed = 42)
     plt.figure()
     nx.draw(G,pos)
         
@@ -120,15 +121,120 @@ def display_graph(graph):
     plt.show()    
 
     
-def display_nxgraph(G):
-    plt.figure(figsize=(20,20))
+def display_nxgraph(G, weighted = False):
+    plt.figure(figsize=(5,5))
     pos = nx.spring_layout(G)  # positions for all nodes
-
-    nx.draw_networkx_nodes(G, pos, node_size=700)
-# edges
-    nx.draw_networkx_edges(G, pos)
-# labels
-    nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+    if (weighted == True) :
+        new_labels = dict(map(lambda x:((x[0],x[1]), str(x[2]['weight'])), G.edges(data = True)))
+    nx.draw_networkx_nodes(G, pos, node_size=400)
+    nx.draw_networkx_labels(G, pos, font_size=15, font_family='sans-serif')
+    if (weighted == True) :
+        nx.draw_networkx_edge_labels(G,pos,edge_labels = new_labels)
+    nx.draw_networkx_edges(G, pos, width=2)
 
     plt.axis('off')
     plt.show()
+
+    
+def longest_list_in_a_list(mainlist):
+    longestlist = []
+    for ls in mainlist:
+        if len(ls)> len(longestlist):
+            longestlist = ls
+    return longestlist
+        
+
+
+def combinations(liste,k):
+    return list(it.combinations(liste, k))
+
+
+def combinations_recursive(graph):
+    nodes = graph.nodes
+    length = len(nodes)
+    combinaisons = []
+    for i in range(1,length+1):
+        combinaisons.extend(combinations(nodes,i))
+    return combinaisons
+    
+
+def find_K(laplacian, min_energy = 0.9):
+   
+    parcours_total = 0.0
+    total = sum(laplacian)
+    
+    if total == 0.0:
+        
+        return len(laplacian)
+    
+    for i in range(len(laplacian)):
+        parcours_total += laplacian[i]
+        if parcours_total/total >= min_energy:
+            return i+1
+        
+    return len(laplacian)
+
+
+def eigenvector_similarity(graph1, graph2):
+    
+    # Calcul des valeurs propres des laplaciens des graphs : 
+    laplacien_1 = nx.spectrum.laplacian_spectrum(graph1)
+    laplacien_2 = nx.spectrum.laplacian_spectrum(graph2)
+    
+    # On trouve le meilleur K pour les deux graphs
+    K_1 = find_K(laplacien_1)
+    K_2 = find_K(laplacien_2)
+    
+    K = min(K_1, K_2)
+
+    distance = sum((laplacien_1[:K]-laplacien_2[:K])**2)
+    return distance
+
+
+def extract_induced_subgraph(graph, list_nodes_tokeep):
+    subgraph = graph.copy()
+    listnodes = [x for x in subgraph.nodes if x not in list_nodes_tokeep]
+    subgraph.remove_nodes_from(listnodes)
+    return subgraph
+
+
+def extract_all_induced_subgraphs(graph,combinaisons):
+    subgraphs = []
+    for combinaison in combinaisons:
+        subgraphs.append(extract_induced_subgraph(graph,combinaison))
+    return subgraphs
+
+def mcs(G1,G2,distance_metrics = 'eigen'):
+    
+    # Combinaisons
+    print("Combinaisons en construction...")
+    nodesG1 = len(G1.nodes)
+    nodesG2 = len(G2.nodes)
+    combinaisons1 = combinations_recursive(G1)
+    combinaisons2 = combinations_recursive(G2)
+    print("Terminé!")
+    
+    # Construction et Stockage des Sous-Graphes Induits
+    print("Extraction des Induced Subgraphs...")    
+    subgraphs1 = []
+    for combinaison in combinaisons1:
+        graph_extracted = extract_induced_subgraph(G1,combinaison) 
+        if nx.is_connected(graph_extracted):
+            subgraphs1.append(graph_extracted)      
+    subgraphs2 = []
+    for combinaison in combinaisons2:
+        graph_extracted = extract_induced_subgraph(G2,combinaison) 
+        if nx.is_connected(graph_extracted):
+            subgraphs2.append(graph_extracted)   
+    print("Terminé!")    
+    
+    # Distances et stockage des sous graphs communs
+    communs = []
+    print("Distances...")
+    for sub1 in subgraphs1:
+        for sub2 in subgraphs2:
+            distance = eigenvector_similarity(sub1,sub2)
+            if (distance == 0.0):
+                communs.append((sub1,sub2,len(sub1.nodes)))
+    print("Terminé!")
+    return communs
